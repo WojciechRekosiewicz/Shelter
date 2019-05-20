014d587db6b935976c788cfd07ff274758b4ed3f
WojciechRekosiewicz <wojtek24a@gmail.com>
2019-05-20 13:37:17 +0200

    start adding models

---
+++ Shelter/Models/Advert.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Shelter.Models
+{
+    public class Advert
+    {
+        public int AdvertId { get; set; }
+        public string Title { get; set; }
+        [ForeignKey("User")]
+        public int AuthorId { get; set; }
+        [ForeignKey("User")]
+        public int ReservingId { get; set; }
+        public string ShortDescription { get; set; }
+        public string LongDescription { get; set; }
+        public string ImageUrl { get; set; }
+
+    }
+}
