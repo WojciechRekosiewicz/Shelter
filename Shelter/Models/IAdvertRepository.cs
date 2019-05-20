1d5afb0283a5a4467a2b5517d4a247cbd8c699e1
WojciechRekosiewicz <wojtek24a@gmail.com>
2019-05-20 14:38:24 +0200

    added repositories

---
+++ Shelter/Models/IAdvertRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shelter.Models
+{
+    interface IAdvertRepository
+    {
+        IEnumerable<Advert> GetAllAdverts();
+        Advert GetAdvertById(int AdvertID);
+    }
+}
---
+++ Shelter/Models/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shelter.Models
+{
+    interface IUserRepository
+    {
+        IEnumerable<User> GetAllUsers();
+        User GetUserById(int UserId);
+    }
+}
