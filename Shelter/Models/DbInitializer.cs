c1f741015f3c4544941066c0cad1956de2474b8a
Gonska93 <gonska9933@gmail.com>
2019-05-21 12:17:01 +0500

    Layout fix and add sample adverts initialization in dbinitializer class

--- Shelter/Models/DbInitializer.cs
+++ Shelter/Models/DbInitializer.cs
@@ -9,6 +9,16 @@ namespace Shelter.Models
     {
         public static void Seed(AppDbContext context)
         {
+            if (!context.Adverts.Any())
+            {
+                context.AddRange
+                (
+                    new Advert { Title = "Kot burek", AuthorId = "eaa198b0-3105-432f-9f14-f5e8d9f669a2", ShortDescription = "Taki se kotek fajny", LongDescription = "Super mega ekstra dlugi kot z dlugim opisem", ImageUrl = "https://img.besty.pl/images/396/82/3968217.jpg" },
+                    new Advert { Title = "Pies Azor", AuthorId = "eaa198b0-3105-432f-9f14-f5e8d9f669a2", ShortDescription = "Duży skurczybyk", LongDescription = "Fajnie goni listonosza", ImageUrl = "https://www.wykop.pl/cdn/c3201142/comment_zKuWohgYtJwjt30l9fjfxDUipsxbVUOj.jpg" }
+                );
+
+                context.SaveChanges();
+            }
         }
     }
 }
