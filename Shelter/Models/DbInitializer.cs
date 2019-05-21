34fee1b278fe9cb365e6cdf25abbaec025653ac4
WojciechRekosiewicz <wojtek24a@gmail.com>
2019-05-21 14:43:13 +0200

    navbar modeled

--- Shelter/Models/DbInitializer.cs
+++ Shelter/Models/DbInitializer.cs
@@ -13,8 +13,8 @@ namespace Shelter.Models
             {
                 context.AddRange
                 (
-                    new Advert { Title = "Kot burek", AuthorId = "eaa198b0-3105-432f-9f14-f5e8d9f669a2", ShortDescription = "Taki se kotek fajny", LongDescription = "Super mega ekstra dlugi kot z dlugim opisem", ImageUrl = "https://img.besty.pl/images/396/82/3968217.jpg" },
-                    new Advert { Title = "Pies Azor", AuthorId = "eaa198b0-3105-432f-9f14-f5e8d9f669a2", ShortDescription = "Duży skurczybyk", LongDescription = "Fajnie goni listonosza", ImageUrl = "https://www.wykop.pl/cdn/c3201142/comment_zKuWohgYtJwjt30l9fjfxDUipsxbVUOj.jpg" }
+                    new Advert { Title = "Kot burek", AuthorId = "d1fb7415-45ca-40d3-86a0-55c258965444", ShortDescription = "Taki se kotek fajny", LongDescription = "Super mega ekstra dlugi kot z dlugim opisem", ImageUrl = "https://img.besty.pl/images/396/82/3968217.jpg" },
+                    new Advert { Title = "Pies Azor", AuthorId = "d1fb7415-45ca-40d3-86a0-55c258965444", ShortDescription = "Duży skurczybyk", LongDescription = "Fajnie goni listonosza", ImageUrl = "https://www.wykop.pl/cdn/c3201142/comment_zKuWohgYtJwjt30l9fjfxDUipsxbVUOj.jpg" }
                 );
 
                 context.SaveChanges();
