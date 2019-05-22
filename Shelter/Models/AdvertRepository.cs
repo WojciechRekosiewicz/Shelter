925d6196705eac5bd4f2872eafa1ea2e2a455a87
Gonska93 <gonska9933@gmail.com>
2019-05-22 09:48:16 +0500

    Modify IAdvertRepository due to adding additional features(Create, Delete) and implementing Creating feature

--- Shelter/Models/AdvertRepository.cs
+++ Shelter/Models/AdvertRepository.cs
@@ -24,6 +24,16 @@ namespace Shelter.Models
             return _appDbContext.Adverts.FirstOrDefault(p => p.AdvertId == AdvertID);
         }
 
+        public void Create(Advert advert)
+        {
+            _appDbContext.Add(advert);
 
+            _appDbContext.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
